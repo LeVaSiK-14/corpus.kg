16c9331b3fb03dc16350fd6aab66c4fbe7b10e94
leva-14 <lev201611@gmail.com>
2022-04-02 15:43:57 +0600

    dockerize project

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,13 @@ ENV PYTHONUNBUFFERED 1
 
 WORKDIR /app
 
-COPY requirements.txt .
+COPY requirements.txt /app/
+ADD wsgi-entrypoint.sh /app/
 
 RUN python -m pip install --upgrade pip
 RUN pip install -r requirements.txt
 
-COPY . .
+COPY . /app/
 EXPOSE 8000
+
+CMD [ "chmod", "+x", "wsgi-entrypoint.sh" ]
--- core/settings.py
+++ core/settings.py
@@ -86,23 +86,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": config("DATABASE_NAME"),
-#         "USER": config("DATABASE_USER"),
-#         "PASSWORD": config("DATABASE_PASSWORD"),
-#         "HOST": config("DATABASE_HOST"),
-#         "PORT": config("DATABASE_PORT"),
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "shopdb",
+        "USER": "shopadmin",
+        "PASSWORD": "shoppassword",
+        "HOST": 'db',
+        "PORT": 5432,
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
